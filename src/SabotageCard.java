577847483ffda0fcc8e02997cb5ae9adff89ad79
Reissa <steve@jobs.com>
2026-05-13 09:30:29 -0500

    debug

--- src/SabotageCard.java
+++ src/SabotageCard.java
@@ -1,4 +1,4 @@
-public class SabotageCard extends AttackCard{
+public class SabotageCard extends AttackCard {
     @Override
     public void doDamage(Player currentPlayer, Player playerToDamage) {
         System.out.println("An enemy nation sabotaged your work!");
